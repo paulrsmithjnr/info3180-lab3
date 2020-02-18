b2510a812ebea1256f9b3b4f1e13be102573840f
Paul Smith <paulrsmithjnr@gmail.com>
2020-02-18 17:55:50 -0500

    Completed Lab 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'passphrase'
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465' # (or try 2525)
+app.config['MAIL_USERNAME'] = 'cc284c4b89c5c6'
+app.config['MAIL_PASSWORD'] = 'ac15b593f650ea'
+mail = Mail(app)
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ This file creates your application.
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
+from app import mail
+from flask_mail import Message 
+
+from .forms import ContactForm
 
 ###
 # Routing for your application.
@@ -22,7 +26,28 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Paul Smith")
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    """Render the website's contact page."""
+    contactPage = ContactForm()
+
+    if request.method == 'POST':
+        if contactPage.validate_on_submit():
+            name = contactPage.name.data
+            email = contactPage.email.data
+            subject = contactPage.subject.data
+            message = contactPage.message.data
+
+            msg = Message(subject, sender=(name, email),recipients=["c3fd27e777-ba79c3@inbox.mailtrap.io"])
+            msg.body = message
+            mail.send(msg) 
+
+            flash('Your email was sent successfully!', 'success')
+            return redirect(url_for('home'))
+        flash_errors(contactPage)
+    return render_template('contact.html', form=contactPage)
 
 
 ###
